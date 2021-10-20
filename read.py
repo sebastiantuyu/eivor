718193624b7797380f5191b53a83a0d04508f07d
sebastiantuyu <alextpinero@gmail.com>
2021-10-19 20:28:50 -0500

    gui-ready

--- read.py
+++ read.py
@@ -12,29 +12,34 @@ def setup():
     eel.start('index.html')
     # open our serial port at 9600 baud
     
-
+# pkg_size is the minimum time, should never be 0
 @eel.expose
-def worker():
-    with serial.Serial(DEV, 9600, timeout=1) as ser:
-    
-        time_ = time.time()
-        position = 0
-        DATA = {}
-        while (10 > time.time() - time_):
-            raw = ser.read(size=2)
+def worker(pkg_size):
+    # Avoid reading strings or values less than 0
+    if type(pkg_size) == int and pkg_size > 0: 
+        with serial.Serial(DEV, 9600, timeout=1) as ser:
         
-            if raw:
-                try:
-                  # read the high and low byte
-                  high, low = raw
-    
-                  # add up our bits from high and low byte
-                  # to get the final value
-                  DATA[position] = high * 256 + low
-                  position+=1
-                except:
-                    pass
-        return json.dumps(DATA)
+            time_ = time.time()
+            position = 0
+            DATA = {}
+            while (pkg_size > time.time() - time_):
+                raw = ser.read(size=2)
+            
+                if raw:
+                    try:
+                        # read the high and low byte
+                        high, low = raw
+            
+                        # add up our bits from high and low byte
+                        # to get the final value
+                        DATA[position] = high * 256 + low
+                        position+=1
+                    except:
+                        pass
+            return json.dumps(DATA)
+    else:
+        print("Error while reading values...",pkg_size)
+        return json.dumps({0 : 0})
 
 if __name__ == "__main__":
     setup()
--- src/script.js
+++ src/script.js
@@ -1,42 +1,117 @@
 
+let pastN = 0
+let pkgSize = 3
+let avCache = []
+let isReading = true 
+let isLoading = false
 const DATA = [["Seconds","Value"]]
-let options = {
-    title: 'Arduino test',
-    curveType: 'function',
-    legend: { position: 'bottom' },
-    vAxis: {title:'Valores',maxVal:1023,minVal:0}
-  };
+const AV_DATA = [["Seconds","Value"]]
+const errorMessage = document.querySelector("#error")
 
-//let data = new google.visualization.DataTable();
-readData()
+const init = () => {
+    const pkgController = document.querySelector("#pkg-controller")
+    const actionButton = document.querySelector("#start-controller")
+    const stopperButton = document.querySelector("#stop-controller")
+    const arValue = document.querySelector("#ar-value")
+
+
+    pkgController.addEventListener("change",(e) => {
+        // Each time that the value is updated 
+        // the worker is stopped
+        isReading = false
+        pkgSize = parseInt(e.target.value)
+        arValue.innerHTML = pkgSize
+
+    })
+
+    actionButton.addEventListener("click",() => {
+        isLoading = true
+        isReading = true
+        actionButton.disabled = true
+        stopperButton.disabled = false
+        readData()
+    })
+
+    stopperButton.addEventListener("click",() => {
+        isReading = false
+        actionButton.disabled = false
+        stopperButton.disabled = true
+    })
+}
+
+
+// Autoinitlize function 
+init()
+
+
 
 async function run() {
-    let results = await eel.worker()();
+    let results = await eel.worker(pkgSize)();
+    if(results === null || results === undefined || results === []){
+        errorMessage.innerHTML = "Arduino se desconecto!"
+    }
+    if(isLoading){ isLoading = false}
     return JSON.parse(results)
 }
 
 async function readData() {
-    console.log("getting data...")
-    const data_ = await run()
-    //const raw_ = []
-    for(d in data_ ){
-        DATA.push([d,data_[d]])
+    console.log("reading data...")
+    google.charts.load('current', {'packages':['corechart']});
+    while(isReading == true) {
+        const data_ = await run()
+        for(d in data_ ){
+            // avoid big numbers noise
+
+            if(data_[d] > 1023) {
+                DATA.push([d,1023])
+                computeAverage(1023)
+            }else {
+                DATA.push([d,data_[d]])
+                computeAverage(data_[d])
+            }
+        }
+        //Array.from(data_).forEach((val,idx) => DATA.push([idx,val]))
+        console.table(data_)
+        google.charts.setOnLoadCallback(drawChart);
     }
     
-
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
 }
 
 
 function drawChart() {
-    google.charts.load('current', {'packages':['corechart']});
-    //data.addColumn('number', 'Seconds');
-    //data.addColumn('number', 'Value');
     var data = google.visualization.arrayToDataTable(DATA);
+    var dataAv = google.visualization.arrayToDataTable(AV_DATA);
+
 
     var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    chart.draw(data, options);
+    var avChart = new google.visualization.LineChart(document.getElementById('av_chart'));
+
+
+    chart.draw(data,{
+        title: 'Real-time',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        vAxis: {title:'Valores',maxVal:1023,minVal:0}
+    });
+
+
+    avChart.draw(dataAv,{
+        title: 'Moving Average',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        vAxis: {title:'Valores',maxVal:1023,minVal:0}
+    })
 }
 
 
+const computeAverage = (val) => {
+    pastN+=1
+    avCache.push(val)
+    
+    if(pastN > 200) {
+        let av = avCache.reduce((a,b) => a + b, 0) / avCache.length;
+        AV_DATA.push([AV_DATA.length+1,av])
+        avCache.shift()
+        
+    }
+}
